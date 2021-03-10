d81bd7094e021cd9d5df780be9e6cdaa3e513bc3
BIUMUMLT-005\User <swapnil.jadhav@conceptbiu.com>
2021-03-10 11:48:17 +0530

    migration file is corrected

--- database/migrations/2021_03_07_063341_candidates.php
+++ database/migrations/2021_03_07_063341_candidates.php
@@ -15,23 +15,24 @@ class Candidates extends Migration
     {
         Schema::create('candidates', function (Blueprint $table) {
 
-        $table->increments(id);
+        $table->increments('id');
         $table->string('first_name',40);
         $table->string('last_name',40);
         $table->string('email',100);
         $table->string('contact_number',15);
-        $table->tinyInteger('gender',4)->nullable()->default('NULL');
+        $table->tinyInteger('gender',4)->unsigned()->nullabe()->default(NULL);
         $table->string('specialization',200)->nullable()->default('NULL');
         $table->integer('work_ex_year',11);
         $table->integer('candidate_dob',11);
         $table->string('address',500)->nullable()->default('NULL');
         $table->string('resume',200)->nullable()->default('NULL');
         $table->timestamp('created_on')->default('CURRENT_TIMESTAMP');
-        $table->integer('created_by',11)->nullable()->default('NULL');
-        $table->integer('modified_by',11)->nullable()->default('NULL');
+        $table->integer('created_by',11)->unsigned()->nullabe()->default(NULL);
+        $table->integer('modified_by',11)->unsigned()->nullabe()->default(NULL);
         $table->datetime('modified_on')->nullable()->default('NULL');
-        $table->primary('id');   
-        }); 
+        $table->primary('id');
+
+        });
     }
 
     /**
